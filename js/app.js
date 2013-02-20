fe79918c2c2f5d3e04a3770ff690ee646d1ff7b2
saleiva <saleiva@gmail.com>
2013-02-20 12:01:18 +0100

    basic layout created

--- js/app.js
+++ js/app.js
@@ -11,7 +11,8 @@ cartodb.createVis('map1', url, {
 
 cartodb.createVis('map2', url, {
       sql: "SELECT cartodb_id, customer_country, amount, (select the_geom_webmercator FROM comercios_08_mwc as c WHERE t.business_id =c.id ) as the_geom_webmercator FROM tx_output_08_mwc as t WHERE t.tx_date_proc > '2012-02-19T14:00:00+01:00' AND t.tx_date_proc < '2012-02-19T16:30:00+01:00'",
-      cartodb_logo: false
+      cartodb_logo: false,
+      zoomControl: false
   })
   .done(function(vis, layers) {
     return;
