8c09c345acfea8cdc5a73fa211c0d470fba1c0b2
saleiva <saleiva@gmail.com>
2013-02-20 18:42:37 +0100

    adding selector

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ var staticMapsURLs = {
   staticDefault: 'http://saleiva2.cartodb.com/api/v1/viz/186/viz.json'
 }
 
+$('.menuBar a.swtichButton').bind('click', function(){toggleMenu()});
+$('#buttonContainer ul li a').bind('click', toggleMaps);
+
 cartodb.createVis('map1', staticMapsURLs.staticDefault, {
       sql: "SELECT cartodb_id, customer_country, amount, (select the_geom_webmercator FROM comercios_08_mwc as c WHERE t.business_id =c.id ) as the_geom_webmercator FROM tx_output_08_mwc as t WHERE t.tx_date_proc > '2012-02-18T14:00:00+01:00' AND t.tx_date_proc < '2012-02-18T16:30:00+01:00'",
       cartodb_logo: false
@@ -28,9 +31,6 @@ cartodb.createVis('map2', staticMapsURLs.staticDefault, {
   mapR.on('moveend', function(e){changeMapState(mapR,mapL)});
   return;
 });
-
-$('.menuBar a.swtichButton').bind('click', function(){toggleMenu()});
-$('#buttonContainer ul li a').bind('click', toggleMaps);
 
 //Applies the same view from src to tgt map
 function changeMapState(src,tgt){
