dd0b8838c5eca867265583f25188fcfb5a966534
saleiva <saleiva@gmail.com>
2013-02-20 16:08:50 +0100

    implements sliding menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 
 
-var url = 'http://saleiva2.cartodb.com/api/v1/viz/186/viz.json';
-cartodb.createVis('map1', url, {
+var MENU_TOGGLE_SIZE = 210;
+
+var showing_menu = false;
+var staticMapsURLs = {
+  staticDefault: 'http://saleiva2.cartodb.com/api/v1/viz/186/viz.json'
+}
+
+$('.menuBar a').bind('click', function(){toggleMenu()});
+
+cartodb.createVis('map1', staticMapsURLs.staticDefault, {
       sql: "SELECT cartodb_id, customer_country, amount, (select the_geom_webmercator FROM comercios_08_mwc as c WHERE t.business_id =c.id ) as the_geom_webmercator FROM tx_output_08_mwc as t WHERE t.tx_date_proc > '2012-02-18T14:00:00+01:00' AND t.tx_date_proc < '2012-02-18T16:30:00+01:00'",
       cartodb_logo: false
   })
@@ -9,7 +17,7 @@ cartodb.createVis('map1', url, {
     return;
   });
 
-cartodb.createVis('map2', url, {
+cartodb.createVis('map2', staticMapsURLs.staticDefault, {
       sql: "SELECT cartodb_id, customer_country, amount, (select the_geom_webmercator FROM comercios_08_mwc as c WHERE t.business_id =c.id ) as the_geom_webmercator FROM tx_output_08_mwc as t WHERE t.tx_date_proc > '2012-02-19T14:00:00+01:00' AND t.tx_date_proc < '2012-02-19T16:30:00+01:00'",
       cartodb_logo: false,
       zoomControl: false
@@ -17,3 +25,18 @@ cartodb.createVis('map2', url, {
   .done(function(vis, layers) {
     return;
   });
+
+//Toggle menubar.
+function toggleMenu(){
+  var _tgt = (showing_menu) ? '-' : '+';
+  var _td = (showing_menu) ? 250 : 0;
+  showing_menu = !showing_menu;
+  $('.moveUpAnimate').animate({
+    bottom: _tgt+'='+MENU_TOGGLE_SIZE},
+    350);
+  $('.expandAnimate').animate({
+    height: _tgt+'='+MENU_TOGGLE_SIZE},
+    350);
+  $('.swtichButton').delay(_td).fadeToggle(150);
+  $('#buttonContainer').delay(_td).fadeToggle(600);
+}
