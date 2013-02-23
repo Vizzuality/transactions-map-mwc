b27a25ba2b54dd6bc4c46e80cb5f9e9d4f6a9ed4
saleiva <saleiva@gmail.com>
2013-02-23 22:14:07 +0100

    fixing loading

--- js/app.js
+++ js/app.js
@@ -15,6 +15,25 @@ var staticMapsURLs = {
   staticDefault: 'js/viz.json'
 }
 
+var opts = {
+  lines: 15, // The number of lines to draw
+  length: 12, // The length of each line
+  width: 8, // The line thickness
+  radius: 50, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  color: '#fff', // #rgb or #rrggbb
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: 'auto', // Top position relative to parent in px
+  left: 'auto' // Left position relative to parent in px
+};
+var spinner = new Spinner(opts).spin();
+document.getElementById('bigImg').appendChild(spinner.el);
 
 /** 
  * map class
@@ -228,8 +247,8 @@ mapL.init(function() {
     var loaded = function() {
       if(!--c) {
         $('#cover').bind('click', toggleCover);
-        $('.loading').hide();
-        $('.preview').fadeIn();
+        spinner.stop();
+        $('.playBtn').fadeIn();
       }
     }
 
@@ -530,4 +549,7 @@ function toggleCover(e){
   }
   animation.toggle();
 }
+
+
+
 
